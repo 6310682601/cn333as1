9ea37f0874552aa38e6c85bab7867159dbd31cdf
Munin Wutthipongworakit <6310682601@student.tu.ac.th>
2023-03-20 19:05:35 +0700

    edit

--- app/src/main/java/com/example/number_guessing_game/MainActivity.kt
+++ app/src/main/java/com/example/number_guessing_game/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.number_guessing_game
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -20,14 +25,26 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.view.ViewCompat.FocusDirection
 import com.example.number_guessing_game.ui.theme.Number_Guessing_GameTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,10 +59,14 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
+var hint: String = "Let's play"
+var count: Int = 0
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Number_Guessing_Game() {
+    var random by remember { mutableStateOf((1..1000).random()) }
+    var number by remember { mutableStateOf(" ") }
+    val num = number.toIntOrNull()
     Column (
         Modifier
             .fillMaxSize()
@@ -61,27 +82,48 @@ fun Number_Guessing_Game() {
             fontSize = 20.sp,
         )
         Spacer(modifier = Modifier.height(275.dp))
-        var amountInput = "Your Guess"
-        TextField(
-            value = amountInput,
-            onValueChange = {}
+        EditNumberField(
+            value = number,
+            onValueChange = { number = it },
         )
         Spacer(modifier = Modifier.height(275.dp))
         Text(
-            text = "Let,s Play",
+            text = hint ,
             color = Color.Gray,
             fontSize = 20.sp
             )
         Spacer(modifier = Modifier.height(30.dp))
         Row() {
-            Button(onClick = { /*TODO*/ }) {
+            Button(
+                onClick = {
+                    if (num != null) {
+                        if (num < random) {
+                            hint = "Hint: It's higher!";
+                            count += 1;
+                        } else if (num > random) {
+                            hint = "Hint: It's lower!";
+                            count += 1;
+                        } else {
+                            hint = "Correct! use $count time to win";
+                        }
+                    }
+                    else {
+                        hint = "opop"
+                    }
+                })
+            {
                 Text(
                     text = "CHECK",
                     fontSize = 20.sp
-                    )
+                )
             }
             Spacer(modifier = Modifier.width(30.dp))
-            Button(onClick = { /*TODO*/ }) {
+            Button(onClick = {
+                random = (1..1000).random();
+                number = " ";
+                hint = "Let's play";
+                count = 0
+            }) {
                 Text(
                     text = "PLAY AGAIN",
                     fontSize = 20.sp
@@ -90,6 +132,45 @@ fun Number_Guessing_Game() {
         }
 
     }
+}
+
+//private fun check(num: Int, random: Int){
+//    if (num < random) {
+//        hint = "Hint: It's higher!";
+//        count += 1;
+//    } else if (num > random) {
+//        hint = "Hint: It's lower!";
+//        count += 1;
+//    } else {
+//        hint = "Correct! use $count time to win";
+//    }
+//}
+
+//private fun reset() {
+//    random = (1..1000).random();
+//    number = 0;
+//    hint = "Let's play";
+//    count = 0
+//}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditNumberField(
+    value: String,
+    onValueChange: (String) -> Unit
+) {
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(
+            stringResource(R.string.your_guess),
+            fontSize = 20.sp,
+        ) },
+        modifier = Modifier.fillMaxWidth(),
+        singleLine = true
+    )
+
 }
 
 @Preview(showBackground = true)
